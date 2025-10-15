1f4ed32d88e84be4df12b6ad8d03525a23ea484f
Dekker <j.j.dekker1@lumc.nl>
2025-10-15 16:40:41 +0200

    add git push

---
+++ storage_monitor/check_molepi_users_projects.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=spacecheck_molepi_work           # Job name
+#SBATCH --output=space_molepi_work.out              # Output file name
+#SBATCH --error=space_molepi_work.err               # Error file name
+#SBATCH --partition=all               # Partition
+#SBATCH --time=96:00:00                 # Time limit
+#SBATCH --nodes=1                       # Number of nodes
+#SBATCH --ntasks-per-node=1             # MPI processes per node
+#SBATCH --mem=16G
+
+cd $1
+
+du -h --max-depth=1 /exports/molepi/MOLEPI_PROJECTS/ | sort -hr > logs/molepi_research_projects.log
+sh git_push.sh molepi_research_projects.log
+du -h --max-depth=1 /exports/molepi/users/ | sort -hr  > logs/molepi_research_users.log
+sh git_push.sh molepi_research_users.log
+#sh git_push.sh
+
+
+
